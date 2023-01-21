144df9bd792820e4285dfb8a232fea1ebe1daad3
madrimovDev <madrimov5014@gmail.com>
2023-01-21 21:12:07 +0500

    axios instance

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,26 @@
-import axios from 'axios'
+import axios, { HeadersDefaults } from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API,
-  timeout: import.meta.env.PROD ? undefined : 2000,
+	baseURL: import.meta.env.VITE_API,
+	timeout: import.meta.env.PROD ? undefined : 2000
 })
 
-export { }
+api.interceptors.request.use(
+	async (config) => {
+		const token = window.localStorage.getItem('accessToken')
+		config.headers = config.headers ?? {}
+
+		if (token) {
+			//@ts-ignore
+			config.headers.Authorization = token
+		}
+
+		return config
+	},
+	(error) => {
+		console.log(error)
+	}
+)
+
+export default api
+
