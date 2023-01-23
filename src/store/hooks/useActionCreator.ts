b2b7b7c9f28bd19bfd6021586ec8d1048af0ac9d
madrimovDev <madrimov5014@gmail.com>
2023-01-23 11:24:43 +0500

    useActionCreator type

--- src/store/hooks/useActionCreator.ts
+++ src/store/hooks/useActionCreator.ts
@@ -1,11 +1,17 @@
 import { useMemo } from 'react'
-import { ActionCreatorsMapObject, bindActionCreators } from '@reduxjs/toolkit'
+import { ActionCreatorsMapObject, AsyncThunk, bindActionCreators } from '@reduxjs/toolkit'
 import useAppDispatch from './useAppDispatch'
 
-const useActionCreator = <Actions extends ActionCreatorsMapObject>(actions: Actions) => {
+type BoundActions<Actions extends ActionCreatorsMapObject> = {
+	[key in keyof Actions]: Actions[key] extends AsyncThunk<any, any, any> ? BoundAsyncThunk<Actions[key]> : Actions[key]
+}
+type BoundAsyncThunk<Thunk extends AsyncThunk<any, any, any>> = (...args: Parameters<Thunk>) => ReturnType<ReturnType<Thunk>>
+
+
+const useActionCreator = <Actions extends ActionCreatorsMapObject>(actions: Actions): BoundActions<Actions> => {
 	const dispatch = useAppDispatch()
 	return useMemo(() => bindActionCreators(actions, dispatch), [])
 }
 
-export default useActionCreator
 
+export default useActionCreator
